9ae20ad878309c94558c3c3be3d349f3ead26313
zhoufubing <zhoufubing@5ifangxin.com>
2018-10-18 16:05:58 +0800

    '创建文章'

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
+import * as moreActions from './actions'
 
 Vue.use(Vuex)
 
 const state = {
   user: ls.getItem('user'),
-  // 添加 auth 来保存当前用户的登录状态
-  auth: ls.getItem('auth')
+  auth: ls.getItem('auth'),
+  // 所有文章状态
+  articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -16,10 +18,14 @@ const mutations = {
     state.user = user
     ls.setItem('user', user)
   },
-  // 添加 UPDATE_AUTH 来更改当前用户的登录状态
   UPDATE_AUTH(state, auth) {
     state.auth = auth
     ls.setItem('auth', auth)
+  },
+  // 更改所有文章的事件类型
+  UPDATE_ARTICLES(state, articles) {
+    state.articles = articles
+    ls.setItem('articles', articles)
   }
 }
 
@@ -33,7 +39,6 @@ const actions = {
     commit('UPDATE_AUTH', false)
     router.push({ name: 'Home', params: { logout: true } })
   },
-  // 更新个人信息
   updateUser({ state, commit }, user) {
     const stateUser = state.user
 
@@ -42,11 +47,28 @@ const actions = {
     }
 
     commit('UPDATE_USER', user)
+  },
+  // 使用对象展开运算符混入 moreActions
+  ...moreActions
+}
+
+const getters = {
+  getArticleById: (state) => (id) => {
+    let articles = state.articles
+
+    if (Array.isArray(articles)) {
+      articles = articles.filter(article => parseInt(id) === parseInt(article.articleId))
+      return articles.length ? articles[0] : null
+    } else {
+      return null
+    }
   }
 }
 
+
 const store = new Vuex.Store({
   state,
+  getters,
   mutations,
   actions
 })
